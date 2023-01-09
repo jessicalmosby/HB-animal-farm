220a97b96d6082fa97e891d2814174b287241a72
Jessica Mosby <jessicamosby@mac.com>
2023-01-09 17:45:49 -0500

    dynamically rendered animals onto page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import './Animal.css';
 
-export default function Animal(props) {
+export default function Animal({ name, says, top, left, type }) {
   return (
-    <div className="animal">
-      <img src="/animals/cow.svg" />
-      <h1></h1>
+    <div className="animal" style={{ top, left }}>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+      <span className="name">{name}</span>
+      <span>{says}</span>
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Main.css';
 import background from '../../background.png';
 import Animal from '../Animal/Animal.js';
+import { animals } from '../../data.js';
 
 export default function Main() {
   return (
     <main style={{ backgroundImage: `url(${background})` }}>
-      <Animal />
+      {animals.map((item) => (
+        <Animal key={item.name} {...item} />
+      ))}
     </main>
   );
 }
