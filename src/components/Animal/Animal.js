c9804b73b131221cbdce696a1c7da303bde28b8d
Jessica Mosby <jessicamosby@mac.com>
2023-01-09 18:29:03 -0500

    added cow sound

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './Animal.css';
 
 export default function Animal({ name, says, top, left, type }) {
+  let audio = new Audio(`${process.env.PUBLIC_URL}/animals/${type}.wav`);
+  const start = () => {
+    audio.play();
+  };
+
   return (
     <div className="animal" style={{ top, left }}>
-      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+      <img onClick={start} alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
       <span className="name">{name}</span>
       <span>{says}</span>
     </div>
