046617a2ea64f59e44c6b162b4f93179df38a95f
Jessica Mosby <jessicamosby@mac.com>
2023-01-09 17:12:30 -0500

    added background img and cow img

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,10 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+
+export default function Animal(props) {
+  return (
+    <div className="animal">
+      <img src="/animals/cow.svg" />
+      <h1></h1>
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Main.css';
+import background from '../../background.png';
+import Animal from '../Animal/Animal.js';
 
 export default function Main() {
-  return <main></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      <Animal />
+    </main>
+  );
 }
